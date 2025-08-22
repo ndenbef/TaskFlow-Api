6e187aa413863489d452f79cb48484a91c7865fa
NDENBE FRANCK IVAN <ndenbef@gmail.com>
2025-08-22 11:27:19 +0100

    Stopped tracking ignored files

--- TaskFlowProjectApi/Program.cs
+++ TaskFlowProjectApi/Program.cs
@@ -1,6 +1,9 @@
 using TaskFlowProjectApi.Models;
 using TaskFlowProjectApi.Services;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,25 @@ builder.Services.Configure<StoreProjectDbSettings>(
     builder.Configuration.GetSection("ProjectStoreDatabase"));
 
 builder.Services.AddScoped<ProjectServices>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
